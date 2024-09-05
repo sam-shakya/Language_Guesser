6d0bdd908e6e2e8dde642a2399196a0124368b52
Sam Shakya <sam.r.shakya@gmail.com>
2024-09-05 09:07:26 -0400

    prettier version w if

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const franc = require("franc")
 const langs = require("langs");
+const colors = require("colors");
 const input = process.argv[2];
 const langCode = franc(input);
+if (langCode === 'und') {
+    console.log("Sorry, Couldn't figure it out! Try with more sample texts".red)
+} else {
+    const language = langs.where("3", langCode);
+    console.log(`Our best guess is: ${language.name}`.green)
+}
 
-const language = langs.where("3", langCode);
-console.log(language.name)
